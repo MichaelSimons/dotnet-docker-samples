415ad1d6a7728737838e62455d026498b1909b49
Michael Simons <msimons@microsoft.com>
2016-10-18 13:53:18 -0500

    cleanup

--- dotnetapp-buildcontainer/build.sh
+++ dotnetapp-buildcontainer/build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Capture the current build and app image, if any, so that we can clean them up when we are done.
-currentBuildImage=$(docker images build-image -q)
-currentAppImage=$(docker images dockerapp -q)
-
 docker build -t build-image -f Dockerfile.build .
 
 # Create a container from our build image so that we can copy the contents out.
@@ -17,11 +13,3 @@ docker build -t dockerapp .
 docker rm build-container
 
 rm -r ./out
-
-if [ -n "$currentBuildImage" ]; then
-    docker rmi $currentBuildImage
-fi
-
-if [ -n "$currentAppImage" ]; then
-    docker rmi $currentAppImage
-fi
