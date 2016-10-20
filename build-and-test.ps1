05e657382b35815ea1bf95fd9397068b49a8f2c1
MichaelSimons <msimons@microsoft.com>
2016-10-20 13:20:48 -0700

    edit

--- build-and-test.ps1
+++ build-and-test.ps1
@@ -9,14 +9,14 @@ Get-ChildItem -Recurse -Filter Dockerfile.nano | sort DirectoryName | foreach {
     pushd $_.DirectoryName
 
     $tag = "$($dockerRepo):" + $_.DirectoryName.Replace($PSScriptRoot, '').TrimStart('\').Replace('\', '-') + "-nanoserver"
-    Write-Host "--- Building ($_.DirectoryName)\Dockerfile.nano to produce $tag ---"
+    Write-Host "--- Building $($_.DirectoryName)\Dockerfile.nano to produce $tag ---"
 
     if (($_.DirectoryName.EndsWith("prod")) -or ($_.DirectoryName.EndsWith("selfcontained"))) {
         dotnet restore
         if (-NOT $?) {
             throw "Failed dotnet restore"
         }
-	dotnet publish -c Release -o out
+	dotnet publish -c Release -o out -r win10-x64
         if (-NOT $?) {
             throw "Failed dotnet build"
         }
