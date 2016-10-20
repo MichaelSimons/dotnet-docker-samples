184fbd848ab12e6d4102f6062ca2d37f60381be1
MichaelSimons <msimons@microsoft.com>
2016-10-20 13:39:14 -0700

    edits

--- build-and-test.ps1
+++ build-and-test.ps1
@@ -1,28 +1,43 @@
+param(
+    [ValidateSet('win', 'linux')]
+    [string]$plat = "win"
+)
+
 Set-StrictMode -Version Latest
 $ErrorActionPreference="Stop"
 
 $dockerRepo="microsoft/dotnet-samples"
 
+$dockerfileName = "Dockerfile"
+if ($plat -eq "win") {
+    $dockerfileName += ".nano"
+    $rid = "win10-x64"
+    $tagSuffix = "-nanoserver"
+}
+else {
+    $rid = "debian.8-x64"
+}
+
 pushd $PSScriptRoot
 
-Get-ChildItem -Recurse -Filter Dockerfile.nano | sort DirectoryName | foreach {
+Get-ChildItem -Recurse -Filter $dockerfileName | sort DirectoryName | foreach {
     pushd $_.DirectoryName
 
-    $tag = "$($dockerRepo):" + $_.DirectoryName.Replace($PSScriptRoot, '').TrimStart('\').Replace('\', '-') + "-nanoserver"
-    Write-Host "--- Building $($_.DirectoryName)\Dockerfile.nano to produce $tag ---"
+    $tag = "$($dockerRepo):" + $_.DirectoryName.Replace($PSScriptRoot, '').TrimStart('\').Replace('\', '-') + $tagSuffix
+    Write-Host "--- Building $($_.DirectoryName)\$dockerfileName to produce $tag ---"
 
     if (($_.DirectoryName.EndsWith("prod")) -or ($_.DirectoryName.EndsWith("selfcontained"))) {
         dotnet restore
         if (-NOT $?) {
             throw "Failed dotnet restore"
         }
-	dotnet publish -c Release -o out -r win10-x64
+	dotnet publish -c Release -o out -r $rid
         if (-NOT $?) {
             throw "Failed dotnet build"
         }
     }
 
-    docker build -t $tag -f Dockerfile.nano .
+    docker build -t $tag -f $dockerfileName .
     if (-NOT $?) {
         throw "Failed docker build"
     }
